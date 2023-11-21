dbb23ae69c5653e1dbbf1fbb2856ea2d20ae45be
jib411 <88179363+jib411@users.noreply.github.com>
2023-11-21 15:48:38 +1100

    Update ChatBot.py

--- ChatBot.py
+++ ChatBot.py
@@ -1,127 +1,47 @@
-from tkinter import *
-import random
+import json
+from difflib import get_close_matches
 
-def next_turn(row, column):
+def load_knowledge_base(file_path: str) -> dict:
+    with open(file_path, 'r') as file:
+        data: dict = json.load(file)
+    return data
 
-    global player
+def save_knowledge_base(file_path: str, data: dict):
+    with open(file_path, 'w') as file:
+        json.dump(data, file, indent=2)
 
-    if buttons[row][column]['text'] == "" and check_winner() is False:
 
-        if player == players[0]:
+def find_best_match(user_question: str, questions: list[str]) -> str | None:
+    matches: list = get_close_matches(user_question, questions, n=1, cutoff=0.6)
+    return matches[0] if matches else None
 
-            buttons[row][column]['text'] = player
+def get_answer_for_question(question: str, knowledge_base: dict) -> str | None:
+    for q in knowledge_base["questions"]:
+        if q["question"] == question:
+            return q["answer"]
 
-            if check_winner() is False:
-                player = players[1]
-                label.config(text=(players[1]+" turn"))
+def chat_bot():
+    knowledge_base: dict = load_knowledge_base('knowledge_base.json')
 
-            elif check_winner() is True:
-                label.config(text=(players[0]+" wins"))
+    while True:
+        user_input: str = input('You: ')
 
-            elif check_winner() == "Tie":
-                label.config(text="Tie!")
+        if user_input.lower() == 'quit':
+            break
 
+        best_match: str | None = find_best_match(user_input, [q["question"] for q in knowledge_base["questions"]])
+
+        if best_match:
+            answer: str = get_answer_for_question(best_match, knowledge_base)
+            print(f'Bot: {answer}')
         else:
+            print('Bot: I don\'t know the answer. Can you teach me?')
+            new_answer: str = input('Type the answer or "skip" to skip: ')
 
-            buttons[row][column]['text'] = player
+            if new_answer.lower() != 'skip':
+                knowledge_base["questions"].append({"question": user_input, "answer": new_answer})
+                save_knowledge_base('knowledge_base.json', knowledge_base)
+                print('Bot: Thank you! I learnt a new response!')
 
-            if check_winner() is False:
-                player = players[0]
-                label.config(text=(players[0]+" turn"))
-
-            elif check_winner() is True:
-                label.config(text=(players[1]+" wins"))
-
-            elif check_winner() == "Tie":
-                label.config(text="Tie!")
-
-def check_winner():
-
-    for row in range(3):
-        if buttons[row][0]['text'] == buttons[row][1]['text'] == buttons[row][2]['text'] != "":
-            buttons[row][0].config(bg="green")
-            buttons[row][1].config(bg="green")
-            buttons[row][2].config(bg="green")
-            return True
-
-    for column in range(3):
-        if buttons[0][column]['text'] == buttons[1][column]['text'] == buttons[2][column]['text'] != "":
-            buttons[0][column].config(bg="green")
-            buttons[1][column].config(bg="green")
-            buttons[2][column].config(bg="green")
-            return True
-
-    if buttons[0][0]['text'] == buttons[1][1]['text'] == buttons[2][2]['text'] != "":
-        buttons[0][0].config(bg="green")
-        buttons[1][1].config(bg="green")
-        buttons[2][2].config(bg="green")
-        return True
-
-    elif buttons[0][2]['text'] == buttons[1][1]['text'] == buttons[2][0]['text'] != "":
-        buttons[0][2].config(bg="green")
-        buttons[1][1].config(bg="green")
-        buttons[2][0].config(bg="green")
-        return True
-
-    elif empty_spaces() is False:
-
-        for row in range(3):
-            for column in range(3):
-                buttons[row][column].config(bg="yellow")
-        return "Tie"
-
-    else:
-        return False
-
-
-def empty_spaces():
-
-    spaces = 9
-
-    for row in range(3):
-        for column in range(3):
-            if buttons[row][column]['text'] != "":
-                spaces -= 1
-
-    if spaces == 0:
-        return False
-    else:
-        return True
-
-def new_game():
-
-    global player
-
-    player = random.choice(players)
-
-    label.config(text=player+" turn")
-
-    for row in range(3):
-        for column in range(3):
-            buttons[row][column].config(text="",bg="#F0F0F0")
-
-
-window = Tk()
-window.title("Tic-Tac-Toe")
-players = ["x","o"]
-player = random.choice(players)
-buttons = [[0,0,0],
-           [0,0,0],
-           [0,0,0]]
-
-label = Label(text=player + " turn", font=('consolas',40))
-label.pack(side="top")
-
-reset_button = Button(text="restart", font=('consolas',20), command=new_game)
-reset_button.pack(side="top")
-
-frame = Frame(window)
-frame.pack()
-
-for row in range(3):
-    for column in range(3):
-        buttons[row][column] = Button(frame, text="",font=('consolas',40), width=5, height=2,
-                                      command= lambda row=row, column=column: next_turn(row,column))
-        buttons[row][column].grid(row=row,column=column)
-
-window.mainloop()
+if __name__ == '__main__':
+    chat_bot()
